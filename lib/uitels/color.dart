511f70f724eba9240c1dcd38a8d40b40359da9a4
jaydeepsharma93 <sharmajaydeep1515@gmail.com>
2024-04-15 16:47:14 +0530

    15/04

---
+++ lib/uitels/color.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+bool dark = true;
+Colors text = ((dark)?Colors.black:Colors.white) as Colors;
+Color color_sigin_and_login = (dark)?Color(0xff059e2e):Color(0xffffffff);
+Color Color_backgrond = Color(0xffC8D2CB);
+
