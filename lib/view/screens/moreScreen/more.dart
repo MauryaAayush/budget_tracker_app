2ad217ae1d0da3f8cbeef5ceb88e61b1938f4ea7
jaydeepsharma93 <sharmajaydeep1515@gmail.com>
2024-04-15 22:10:28 +0530

    15/04

--- lib/view/screens/moreScreen/more.dart
+++ lib/view/screens/moreScreen/more.dart
@@ -92,7 +92,6 @@ class _MoreScreenState extends State<MoreScreen> {
                             ]),
                         child: Row(
                           children: [
-
                             Padding(
                               padding:
                                   const EdgeInsets.symmetric(horizontal: 20),
