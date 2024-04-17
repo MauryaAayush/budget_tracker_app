ce3a3f2d17fb6fd5f3a8b60e4b740374d938bd22
ashup <ashupal0089@gmail.com>
2024-04-17 10:28:34 +0530

    15-4-24

--- lib/view/screens/homeScreen/home.dart
+++ lib/view/screens/homeScreen/home.dart
@@ -1,6 +1,9 @@
+import 'package:budget_tracker_app/view/screens/homeScreen/providers/homeProvider.dart';
 import 'package:budget_tracker_app/view/screens/log_in/login.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import '../../../utils/color.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,12 +16,15 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    HomeProvider homeProvidertrue = Provider.of(context, listen: true);
+    HomeProvider homeProviderfalse = Provider.of(context, listen: false);
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     DateTime dateTime = DateTime.now();
 
     return Scaffold(
       backgroundColor: Colors.white,
+      bottomNavigationBar: bottem_navigation_bar(),
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
         child: Column(
@@ -29,7 +35,6 @@ class _HomeScreenState extends State<HomeScreen> {
                   margin: const EdgeInsets.fromLTRB(15, 38, 10, 0),
                   height: height * 0.08,
                   width: width * 0.918,
-
                   child: Row(
                     // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -48,8 +53,8 @@ class _HomeScreenState extends State<HomeScreen> {
                               (dateTime.hour >= 0 && dateTime.hour < 12)
                                   ? '  Good Morning 👋'
                                   : (dateTime.hour >= 12 && dateTime.hour < 17)
-                                  ? '  Good Afternoon 👋'
-                                  : '  Good evening 👋',
+                                      ? '  Good Afternoon 👋'
+                                      : '  Good evening 👋',
                               style: TextStyle(
                                 color: Colors.black,
                               ),
@@ -88,7 +93,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   Text(
                     'This month',
                     style:
-                    TextStyle(fontWeight: FontWeight.w600, fontSize: 17.5),
+                        TextStyle(fontWeight: FontWeight.w600, fontSize: 17.5),
                   ),
                   Spacer(),
                   IconButton(
@@ -224,10 +229,10 @@ class _HomeScreenState extends State<HomeScreen> {
                   color: Colors.black12),
               child: Center(
                   child: Text(
-                    'Balance: \$0.00',
-                    style: TextStyle(
-                        fontSize: height / 50, fontWeight: FontWeight.w500),
-                  )),
+                'Balance: \$0.00',
+                style: TextStyle(
+                    fontSize: height / 50, fontWeight: FontWeight.w500),
+              )),
             ),
             SizedBox(
               height: 30,
@@ -550,73 +555,123 @@ class _HomeScreenState extends State<HomeScreen> {
               height: 20,
             ),
             Container(
-              height: height/5,
-              width: width/1.060,
+              height: height / 5,
+              width: width / 1.060,
+              padding: EdgeInsets.only(top: 20, bottom: 20, left: 20),
               decoration: BoxDecoration(
-                color: Colors.white,
                 borderRadius: BorderRadius.circular(10),
+                gradient: LinearGradient(
+                  begin: Alignment(0.0, -1.0), // top to bottom
+                  end: Alignment(0.0, 1.0), // top to bottom
+                  colors: [
+                    Colors.white,
+                    Colors.grey.shade500, // Light grey as an intermediate color
+                  ],
+                  stops: [
+                    0.3,
+                    1,
+                  ], // white ends at 70%, black starts at 80%
+                ),
                 boxShadow: [
                   BoxShadow(
-                    offset: Offset(0, 1),
-                    spreadRadius: 1,
-                    blurRadius: 0.5,
-                    color: Colors.black12,
+                    offset: Offset(2, 5),
+                    blurRadius: 17,
+                    color: Colors.black.withOpacity(0.6),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '    ${homeProviderfalse.formatTimer(homeProvidertrue.secondsRemaining)}',
+                        style: TextStyle(
+                            fontSize: height / 24, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '💎',
+                        style: TextStyle(
+                            fontSize: height / 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.amber),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        ' hours ',
+                        style: TextStyle(
+                            fontSize: height / 85,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey.shade700),
+                      ),
+                      Text(
+                        ' minutes ',
+                        style: TextStyle(
+                            fontSize: height / 85,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey.shade700),
+                      ),
+                      Text(
+                        ' seconds',
+                        style: TextStyle(
+                            fontSize: height / 85,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.grey.shade700),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Exdusive Offer',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              'Limited Time Only-Act Fast!',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Container(
+                          height: height / 20,
+                          width: width / 4,
+                          margin: EdgeInsets.only(left: width / 18),
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: Text(
+                            'Claim',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 18),
+                          ),
+                        )
+                      ],
+                    ),
                   )
                 ],
-              ),child: Column(
-              children: [
-                Row(
-
-                  children: [
-                    SizedBox(width: 130,),
-                    Text('05:20:25',style: TextStyle(
-                        fontSize: height/24,
-                        fontWeight: FontWeight.bold
-                    ),),
-                    Spacer(),
-                    Text('💎   ',style: TextStyle(
-                        fontSize: height/24,
-                        fontWeight: FontWeight.bold
-                    ),),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20,top: 40),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Exdusive Offer',style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),),
-                          Text('Limited Time Only-Act Fast!',style: TextStyle(
-                            fontWeight: FontWeight.w400,
-                            fontSize: 18,
-                          ),),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 50,left: 18),
-                      child: Container(
-                        height: height/20,
-                        width: width/4,
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(15),
-                        ),child: Center(child: Text('Clami',style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 18
-                      ),),),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
+              ),
             ),
 
             SizedBox(
@@ -782,12 +837,12 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                     child: Center(
                         child: Text(
-                          'Upgrade Now',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: height / 45,
-                              fontWeight: FontWeight.w600),
-                        )),
+                      'Upgrade Now',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: height / 45,
+                          fontWeight: FontWeight.w600),
+                    )),
                   ),
                 ],
               ),
@@ -983,5 +1038,202 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-
+  Container bottem_navigation_bar() {
+    return Container(
+      height: 120,
+      width: 500,
+      child: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
+            child: Stack(
+              children: [
+                Container(
+                  height: 60,
+                  width: 450,
+                  decoration: BoxDecoration(
+                    color: (dark) ? Colors.white : Colors.black,
+                  ),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 10,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            hometoch = true;
+                            accounttoch = false;
+                            moretoch = false;
+                            analytoch = false;
+                          });
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.home_rounded,
+                              size: 30,
+                              color: (hometoch)
+                                  ? (dark)
+                                      ? Colors.black
+                                      : Colors.white
+                                  : Colors.grey,
+                            ),
+                            Text(
+                              'Home',
+                              style: TextStyle(
+                                  color: (hometoch) ? text : Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Spacer(),
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            analytoch = true;
+                            hometoch = false;
+                            accounttoch = false;
+                            moretoch = false;
+                          });
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.bar_chart_outlined,
+                                size: 35,
+                                color: (analytoch)
+                                    ? (dark)
+                                        ? Colors.black
+                                        : Colors.white
+                                    : Colors.grey),
+                            Text(
+                              'Analysis',
+                              style: TextStyle(
+                                  color: (analytoch) ? text : Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Spacer(),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            accounttoch = true;
+                            analytoch = false;
+                            hometoch = false;
+                            moretoch = false;
+                          });
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.home_work_outlined,
+                                size: 35,
+                                color: (accounttoch)
+                                    ? (dark)
+                                        ? Colors.black
+                                        : Colors.white
+                                    : Colors.grey),
+                            Text(
+                              'Accounts',
+                              style: TextStyle(
+                                  color: (accounttoch) ? text : Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Spacer(),
+                      InkWell(
+                        onTap: () {
+                          setState(() {
+                            moretoch = true;
+                            accounttoch = false;
+                            analytoch = false;
+                            hometoch = false;
+                          });
+                        },
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.more_horiz_outlined,
+                                size: 35,
+                                color: (moretoch)
+                                    ? (dark)
+                                        ? Colors.black
+                                        : Colors.white
+                                    : Colors.grey),
+                            Text('More',
+                                style: TextStyle(
+                                    color: (moretoch) ? text : Colors.grey)),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(170, 30, 10, 0),
+            child: Container(
+              height: 75,
+              width: 75,
+              decoration: BoxDecoration(
+                color: Color(0xffF0F1F3),
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 1),
+                    spreadRadius: 0.5,
+                    blurRadius: 1,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(175, 35, 40, 0),
+            child: InkWell(
+              onTap: () {
+                Navigator.of(context).pushNamed('/analytics');
+              },
+              child: Container(
+                height: 63,
+                width: 63,
+                decoration: BoxDecoration(
+                  color: (dark) ? Colors.black : Colors.white,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 1.5),
+                      spreadRadius: 0.5,
+                      blurRadius: 2,
+                      color: Colors.black54,
+                    )
+                  ],
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.add,
+                    color: (dark) ? Colors.white : Colors.black,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
